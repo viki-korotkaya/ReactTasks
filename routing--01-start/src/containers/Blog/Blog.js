872e092b2c04429d79eb230775b072a836b556cd
viki-korotkaya <viki.korotkaya@gmail.com>
2019-05-16 17:52:43 -0400

    Routing project and assignment

---
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import './Blog.css';
+import Posts from './Posts/Posts';
+import {Route, NavLink, Switch, Redirect} from "react-router-dom";
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from "./NewPost/NewPost";
+// import FullPost from './FullPost/FullPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
+class Blog extends Component {
+    state = {
+        auth: true
+    }
+    render () {
+
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}
+                            >Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                         </ul>
+                    </nav>
+                </header>
+                <Switch>
+                    {this.state.auth? <Route path="/new-post" component={AsyncNewPost} /> : null}
+                    <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found</h1>} />
+                    {/*<Redirect from="/" to="/posts" />*/}
+                    {/*<Route path="/" component={Posts} />*/}
+                    {/*<Route path="/:id" exact component={FullPost} />*/}
+                </Switch>
+             </div>
+        );
+    }
+}
+
+export default Blog;
